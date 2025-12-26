1274bfc777dff7c528b8ce370dd22bdbb2951863
Blessing-bit-ui <yembeblessing27@gmail.com>
2025-12-26 05:38:33 -0800

    created the cities file and called the Api

--- maplocation/src/pages/Cities.js
+++ maplocation/src/pages/Cities.js
@@ -1,12 +1,25 @@
- 
- const Base_Url = "http://localhost:8000/cities";
+ import {useEffect, useState} from "react"
+ const Base_Url = "http://localhost:8000";
 
 function Cities() {
+const [cities, setCities] =useState([])
+ useEffect(function(){
+ async function fetchCities(){
+const res= await fetch (`${Base_Url}/cities`)
+const data = await res.json()
+setCities(data)
+}
+fetchCities()
+ },[cities])
 
     return (
-        <div>
-            
-        </div>
+       <div>
+        {cities.map((city)=>(
+            <li key={city.id}>
+              {city.cityName}
+            </li>
+        ))}
+       </div>
     )
 }
 
